140fa2dc766c48452dccb24ffed581b1641f68a6
GeorgCantor <cupsman1986@gmail.com>
2025-07-15 17:08:20 +0300

    Add close button

--- app/src/main/java/com/example/todoapp/presentation/screens/NewsDetailScreen.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/NewsDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -55,10 +56,15 @@ fun NewsDetailScreen(
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = {
-                        navController.popBackStack()
-                    }) {
+                    IconButton(onClick = { navController.popBackStack() }) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                actions = {
+                    if (showZoomedImage) {
+                        IconButton(onClick = { showZoomedImage = false }) {
+                            Icon(Icons.Default.Close, contentDescription = "Close")
+                        }
                     }
                 },
                 scrollBehavior = scrollBehavior
