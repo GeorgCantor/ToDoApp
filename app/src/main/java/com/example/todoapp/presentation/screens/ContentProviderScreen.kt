d05e4d259a964afbfcb915b32c840e70118ec218
GeorgCantor <cupsman1986@gmail.com>
2025-10-02 18:47:06 +0300

    Change icon

--- app/src/main/java/com/example/todoapp/presentation/screens/ContentProviderScreen.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/ContentProviderScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -277,7 +277,7 @@ fun NoteCard(
                 Row {
                     IconButton(onClick = onToggleComplete) {
                         Icon(
-                            Icons.Default.Edit,
+                            Icons.Default.Done,
                             contentDescription = "Toggle Complete",
                             tint =
                                 if (note.isCompleted) {
