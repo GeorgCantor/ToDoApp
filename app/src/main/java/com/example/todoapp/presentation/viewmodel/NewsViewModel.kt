3353c8dec4beca387c4c536e67d372c1d1876660
GeorgCantor <cupsman1986@gmail.com>
2025-10-03 20:19:09 +0300

    Fix getting article

--- app/src/main/java/com/example/todoapp/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/viewmodel/NewsViewModel.kt
@@ -3,7 +3,6 @@ package com.example.todoapp.presentation.viewmodel
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
@@ -11,8 +10,6 @@ import androidx.paging.cachedIn
 import com.example.todoapp.domain.model.NewsArticle
 import com.example.todoapp.domain.usecase.GetTopHeadlinesUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 class NewsViewModel(
     getTopUseCase: GetTopHeadlinesUseCase,
@@ -22,16 +19,7 @@ class NewsViewModel(
     private val _scrollState = mutableStateOf<LazyListState?>(null)
     val scrollState: State<LazyListState?> get() = _scrollState
 
-    private val _isRefreshing = MutableStateFlow(false)
-    val isRefreshing = _isRefreshing.asStateFlow()
-
     fun saveScrollState(state: LazyListState) {
         _scrollState.value = state
     }
-
-    fun refresh() {
-        _isRefreshing.value = false
-    }
-
-    fun getNewsById(id: Int): Flow<NewsArticle?> = snapshotFlow { null }
 }
