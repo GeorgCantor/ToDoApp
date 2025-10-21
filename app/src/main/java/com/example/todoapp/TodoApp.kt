eb988489aab5934cfe91369f30a0944fde7e48fc
GeorgCantor <cupsman1986@gmail.com>
2025-10-21 17:23:41 +0300

    Add StrictMode

--- app/src/main/java/com/example/todoapp/TodoApp.kt
+++ app/src/main/java/com/example/todoapp/TodoApp.kt
@@ -1,6 +1,8 @@
 package com.example.todoapp
 
 import android.app.Application
+import android.os.Build
+import android.os.StrictMode
 import com.example.todoapp.data.cache.NewsCache
 import com.example.todoapp.di.appModule
 import org.koin.android.ext.koin.androidContext
@@ -9,6 +11,7 @@ import org.koin.core.context.startKoin
 class TodoApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        setupStrictMode()
 
         startKoin {
             androidContext(this@TodoApp)
@@ -16,5 +19,40 @@ class TodoApp : Application() {
         }
 
         NewsCache.init(this)
+    }
+
+    private fun setupStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy
+                    .Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .build(),
+            )
+
+            val vmPolicyBuilder =
+                StrictMode.VmPolicy
+                    .Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .detectActivityLeaks()
+                    .detectLeakedRegistrationObjects()
+                    .detectFileUriExposure()
+                    .penaltyLog()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vmPolicyBuilder.detectContentUriWithoutPermission()
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                vmPolicyBuilder.detectNonSdkApiUsage()
+            }
+
+            StrictMode.setVmPolicy(vmPolicyBuilder.build())
+        }
     }
 }
